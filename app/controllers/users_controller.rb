1615647f09a9009aa6a2a11ad501910c36c154eb
usskr <rei.takagi731@gmail.com>
2021-12-21 15:01:32 +0000

    ユーザー新規登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "TalkAboutへようこそ!"
+      redirect_to @user
     else
       render 'new'
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   private
 
     def user_params
