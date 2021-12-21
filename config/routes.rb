d07f4c5fc6286a3a723f943981dddeca3d2f69d2
usskr <rei.takagi731@gmail.com>
2021-12-21 11:04:13 +0000

    users_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  get 'homes/help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "homes#top"
+  get "help" => "homes#help"
+  get "signup" => "users#new"
 end
