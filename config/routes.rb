7a22361e0a7d5c8d52f8cf2bacab5377a14cd12d
usskr <rei.takagi731@gmail.com>
2021-12-22 09:58:34 +0000

    Sessions new作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "homes#top"
   get "help" => "homes#help"
   get "signup" => "users#new"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   resources :users
 end
