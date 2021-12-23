568f9026be8a74f3b0904017a149e495f9414b31
usskr <rei.takagi731@gmail.com>
2021-12-23 07:07:43 +0000

    マイページ/ユーザー編集ページ作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
-  
+
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = "ログインしました"
       redirect_to user
     else
-      flash.now[:danger] = "ユーザー名/パスワードが正しくありません"
+      flash[:danger] = "ユーザー名/パスワードが誤っています"
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
