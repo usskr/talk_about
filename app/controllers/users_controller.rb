12e783c4f8fc2cdbd94b569b11622c4c1a3a34e7
usskr <rei.takagi731@gmail.com>
2021-12-23 07:21:52 +0000

    閲覧・編集制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -7,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "TalkAboutへようこそ!"
+      flash[:success] = "TalkAbout_へようこそ!"
       redirect_to @user
     else
       render 'new'
@@ -37,5 +40,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_path
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
