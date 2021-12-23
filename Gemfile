8df9e18cca9f7d7e7f52eb4585a6fe635687ac5e
usskr <rei.takagi731@gmail.com>
2021-12-23 12:42:57 +0000

    サンプルユーザー作成

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ gem 'font-awesome-sass', '~> 5.13'
 gem 'rails-i18n'
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "tatata",
+             email: "tatata@example.com",
+             password: "tatata",
+             password_confirmation: "tatata")
+
+10.times do |n|
+  name  = Faker::JapaneseMedia::OnePiece.character
+  email = "test-#{n+1}@example.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
